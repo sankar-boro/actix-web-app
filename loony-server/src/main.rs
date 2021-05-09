e1417cc20075e48c07f7c3a1b3e4ef1ad94a28a5
Sankar boro <sankar.boro@yahoo.com>
2021-05-09 09:52:51 +0530

    Scylla driver working.

--- loony-server/src/main.rs
+++ loony-server/src/main.rs
@@ -1,7 +1,29 @@
-use loony_pg;
+use std::sync::Arc;
+
+use anyhow::Result;
+use loony_scylla::{route, App};
+use scylla::{Session, SessionBuilder};
+use actix_web::{App as ActixApp, HttpServer};
+use scylla::transport::session::{IntoTypedRows};
+
+
+async fn session() -> Session {
+    let uri = "127.0.0.1:9042";
+    SessionBuilder::new().known_node(uri).build().await.unwrap()
+}
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    loony_pg::start_pg_app().await.unwrap();
+async fn main() -> Result<()> {
+    let session = session().await;
+    let app = App::new(Arc::new(session));
+
+    HttpServer::new(move || {
+        ActixApp::new()
+            .data(app.clone())
+            .configure(route::routes)
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await?;
     Ok(())
 }
