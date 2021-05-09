7da20ad539a185e51dec2ab9f2778c4e640e6c1b
Sankar boro <sankar.boro@yahoo.com>
2021-05-09 10:27:25 +0530

    Created pool builder for Scylla connection.

--- loony-server/src/main.rs
+++ loony-server/src/main.rs
@@ -1,29 +1,6 @@
-use std::sync::Arc;
-
-use anyhow::Result;
-use loony_scylla::{route, App};
-use scylla::{Session, SessionBuilder};
-use actix_web::{App as ActixApp, HttpServer};
-use scylla::transport::session::{IntoTypedRows};
-
-
-async fn session() -> Session {
-    let uri = "127.0.0.1:9042";
-    SessionBuilder::new().known_node(uri).build().await.unwrap()
-}
+use loony_scylla::start_scylla_app;
 
 #[actix_web::main]
-async fn main() -> Result<()> {
-    let session = session().await;
-    let app = App::new(Arc::new(session));
-
-    HttpServer::new(move || {
-        ActixApp::new()
-            .data(app.clone())
-            .configure(route::routes)
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await?;
-    Ok(())
+async fn main() {
+    start_scylla_app().await.unwrap();
 }
