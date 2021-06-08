bc29021786bd6f778bb422dad754cf662b2a9aaa
Sankar boro <sankar.boro@yahoo.com>
2021-06-08 21:08:32 +0530

    implementations doubts and queries

--- lily-scylla/src/user/update.rs
+++ lily-scylla/src/user/update.rs
@@ -1,10 +1,24 @@
+use uuid::Uuid;
 use serde::Deserialize;
 use actix_web::{web, HttpResponse};
-use crate::{App, AppError, utils::{ConnectionResult, GetQueryResult, Update}};
+use serde::{Serialize};
+
+// both of them is required to implement FromRow
+use scylla::macros::FromRow;
+use scylla::frame::response::cql_to_rust::FromRow;
+
+use crate::{App, utils::{ConnectionResult, GetQueryResult, Update}};
 
 #[derive(Deserialize)]
 pub struct UpdateUserData {
     fname: String,
+}
+
+#[derive(FromRow, Serialize)]
+pub struct User {
+	id: Uuid,
+	email: String,
+	password: Vec<u8>,
 }
 
 pub async fn update_one(session: web::Data<App>, id: web::Path<String>, request: web::Form<UpdateUserData>) 
@@ -14,15 +28,8 @@ pub async fn update_one(session: web::Data<App>, id: web::Path<String>, request:
             .set("fname", &request.fname)
             .where_in("userid", &id)
             .query();
-    let res = conn
+    let _: Option<Vec<User>> = conn
     .query(query, &[])
-    .await;
-    match res {
-        Ok(_) => {
-            Ok(HttpResponse::Ok().body("User updated"))
-        }
-        Err(err) => {
-            return Err(AppError::from(err).into());
-        }
-    }
+    .await.get_query_result()?;
+    Ok(HttpResponse::Ok().body("User updated"))
 }
