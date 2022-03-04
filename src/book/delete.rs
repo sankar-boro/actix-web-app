98186df175fd249ff28bbec982a5e336a25efb0b
Sankar boro <sankar.boro@yahoo.com>
2022-03-04 14:33:27 +0530

    batch delete

--- src/book/delete.rs
+++ src/book/delete.rs
@@ -84,14 +84,20 @@ pub async fn deleteLastNode(
     Ok(HttpResponse::Ok().body("Deleted."))
 }
 
+pub static DELETE_BOOK: &str = "DELETE * FROM sankar.book where bookId=?";
+pub static DELETE_BOOK_INFO: &str = "DELETE * FROM sankar.bookInfo where bookId=?";
+
 pub async fn deleteBook(
     app: web::Data<App>,
-    book_id: web::Path<String>
+    bookId: web::Path<String>
 ) -> Result<HttpResponse, crate::AppError> {
-    let book_id = Uuid::parse_str(&book_id)?;
+    let book_id = Uuid::parse_str(&bookId)?;
 
-    // make delete query
-    let delete_query = format!("DELETE * FROM sankar.book WHERE bookId={}", &book_id);
-    app.query(delete_query, &[]).await?;
+    let mut batch: Batch = Default::default();
+    batch.append_statement(DELETE_BOOK);
+    batch.append_statement(DELETE_BOOK_INFO);
+    
+    let batch_values = ((&book_id,), (&book_id,));
+    app.batch(&batch, &batch_values).await?;
     Ok(HttpResponse::Ok().body("Deleted book."))
 }
