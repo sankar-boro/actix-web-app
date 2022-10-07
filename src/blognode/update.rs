c9d2417aea8069afba51be9254a9661b490da0c4
sankar.boro <sankar.boro@yahoo.com>
2022-10-07 18:33:14 +0530

    everything was ok.

--- src/blognode/update.rs
+++ src/blognode/update.rs
@@ -4,10 +4,6 @@ use crate::App;
 use validator::Validate;
 use scylla::macros::FromRow;
 use uuid::Uuid;
-use actix_session::Session;
-use crate::auth::AuthSession;
-use scylla::batch::Batch;
-use scylla::query::Query;
 
 #[derive(Deserialize, Validate, FromRow)]
 pub struct UpdateRequest {
@@ -15,37 +11,17 @@ pub struct UpdateRequest {
     body: String,
     blogId: String,
     uniqueId: String,
-    category: String,
 }
 
 pub async fn update(
     app: web::Data<App>, 
-    payload: web::Json<UpdateRequest>,
-    session: Session
+    payload: web::Json<UpdateRequest>
 ) 
 -> Result<HttpResponse, crate::AppError> 
 {   
     let blogId = Uuid::parse_str(&payload.blogId)?;
     let uniqueId = Uuid::parse_str(&payload.uniqueId)?;
-    let auth = session.user_info()?;
-    let auth_id = Uuid::parse_str(&auth.userId)?;
-
-    let mut batch: Batch = Default::default();
-    let blogQuery = Query::from(format!("UPDATE sankar.blog SET title=?, body=? WHERE blogId=? AND uniqueId=?"));
-    let blogsQuery = Query::from(format!("UPDATE sankar.blogs SET title=?, body=? WHERE blogId=?"));
-    let userBlogsQuery = Query::from(format!("UPDATE sankar.userblogs SET title=?, body=? WHERE authorId=? AND blogId=?"));
-    let categoryBlogsQuery = Query::from(format!("UPDATE sankar.categoryblogs SET title=?, body=? WHERE category=? AND blogId=?"));
-
-    batch.append_statement(blogQuery);
-    batch.append_statement(blogsQuery);
-    batch.append_statement(userBlogsQuery);
-    batch.append_statement(categoryBlogsQuery);
-    app.batch(&batch, (
-        (&payload.title, &payload.body, &blogId, &uniqueId),
-        (&payload.title, &payload.body, &blogId),
-        (&payload.title, &payload.body, &auth_id, &blogId),
-        (&payload.title, &payload.body, &payload.category, &blogId),
-    )).await?;
-
+    let query = format!("UPDATE sankar.blog SET title=?, body=? WHERE blogId=? AND uniqueId=?");
+    app.query(query, (&payload.title, &payload.body, &blogId, &uniqueId)).await?;
     Ok(HttpResponse::Ok().body("Updated".to_string()))
 }
--- src/booknode/update.rs
+++ src/booknode/update.rs
@@ -4,10 +4,6 @@ use crate::App;
 use validator::Validate;
 use scylla::macros::FromRow;
 use uuid::Uuid;
-use actix_session::Session;
-use crate::auth::AuthSession;
-use scylla::batch::Batch;
-use scylla::query::Query;
 
 #[derive(Deserialize, Validate, FromRow)]
 pub struct UpdateRequest {
@@ -15,37 +11,19 @@ pub struct UpdateRequest {
     body: String,
     bookId: String,
     uniqueId: String,
-    category: String
 }
 
 pub async fn update(
     app: web::Data<App>, 
-    payload: web::Json<UpdateRequest>,
-    session: Session
+    payload: web::Json<UpdateRequest>
 ) 
 -> Result<HttpResponse, crate::AppError> 
 {   
     let bookId = Uuid::parse_str(&payload.bookId)?;
     let uniqueId = Uuid::parse_str(&payload.uniqueId)?;
-    let auth = session.user_info()?;
-    let auth_id = Uuid::parse_str(&auth.userId)?;
 
-    let mut batch: Batch = Default::default();
-    let bookQuery = Query::from(format!("UPDATE sankar.book SET title=?, body=? WHERE bookId=? AND uniqueId=?"));
-    let booksQuery = Query::from(format!("UPDATE sankar.books SET title=?, body=? WHERE bookId=?"));
-    let userBooksQuery = Query::from(format!("UPDATE sankar.userbooks SET title=?, body=? WHERE authorId=? AND bookId=?"));
-    let categoryBooksQuery = Query::from(format!("UPDATE sankar.categorybooks SET title=?, body=? WHERE category=? AND bookId=?"));
-
-    batch.append_statement(bookQuery);
-    batch.append_statement(booksQuery);
-    batch.append_statement(userBooksQuery);
-    batch.append_statement(categoryBooksQuery);
-    app.batch(&batch, (
-            (&payload.title, &payload.body, &bookId, &uniqueId),
-            (&payload.title, &payload.body, &bookId),
-            (&payload.title, &payload.body, &auth_id, &bookId),
-            (&payload.title, &payload.body, &payload.category, &bookId),
-        )
-    ).await?;
+    let query = format!("UPDATE sankar.book SET title=?, body=? WHERE bookId=? AND uniqueId=?");
+    app.query(query, (&payload.title, &payload.body, &bookId, &uniqueId)).await?;
+    
     Ok(HttpResponse::Ok().body("Updated".to_string()))
 }
